4df1992bee0e4bbd94be57817418f50c6e3692e1
Yanislav Pichugin <43216422+Manshooo@users.noreply.github.com>
2026-05-21 04:12:12 +0300

    v0.0.1
    Проделано много первичной работы. Есть менюшка, можно загрузиться в игру. Можно билдить. Базовый мувмент персонажа, базовая менюшка, настроен Git (LFS)

---
+++ entities/player/player.gd
@@ -0,0 +1,42 @@
+# entities/player/player.gd
+extends CharacterBody3D
+class_name PlayerEntity
+
+@export var health : HealthComponent
+# @export var movement : MovementComponent
+
+const SPEED: float = 5.0
+const JUMP_VELOCITY: float = 4.5
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+func toggle_mouse() -> void:
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("capture_mouse"):
+		toggle_mouse()
