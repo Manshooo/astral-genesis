8179f358029c6070b7fe8b7aac9325dd633367d5
Yanislav Pichugin <43216422+Manshooo@users.noreply.github.com>
2026-08-13 17:32:51 +0300

    первые тесты ragdoll анимации

--- src/entities/body/e_body_hound.gd
+++ src/entities/body/e_body_hound.gd
@@ -1,2 +1,7 @@
 @tool
 extends Entity
+
+@onready var physical_bone_simulator: PhysicalBoneSimulator3D = $Skeleton3D/PhysicalBoneSimulator3D
+
+func _ready():
+	physical_bone_simulator.physical_bones_start_simulation()
